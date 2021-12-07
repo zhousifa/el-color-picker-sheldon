71a58d87a1472c92ecc98f4b7f8308a59c396efa
zhousifa <884701384@qq.com>
2021-12-07 10:34:08 +0800

    add message

--- dev/serve.js
+++ dev/serve.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
-import {Slider,Switch,Row,Col,Button,Input} from 'element-ui'
+import {Slider,Switch,Row,Col,Button,Input,Message,MessageBox} from 'element-ui'
 Vue.use(Slider)
 Vue.use(Switch)
 Vue.use(Row)
 Vue.use(Col)
 Vue.use(Button)
 Vue.use(Input)
+Vue.prototype.$message = Message
+Vue.prototype.$confirm = MessageBox.confirm
 import 'element-ui/lib/theme-chalk/index.css';
 import Dev from './serve.vue';
 
